18452c7a5797ec1eaa72b3a436d828b27c74a570
Amna <amna.bashir2@gmail.com>
2022-01-11 20:41:53 +0000

    All tests done

---
+++ src/tests/Calculator.test.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Calculator from '../containers/Calculator';
+import {mount, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe('Calculator', () => {
+  let container;
+  beforeEach(() => {
+    container = mount(<Calculator/>)
+  })
+
+  it('should change running total on number enter', () => {
+    const button4 = container.find('#number4');
+    const runningTotal = container.find('#running-total');
+    button4.simulate('click');
+    expect(runningTotal.text()).toEqual('4');
+  })
+  it('should add one to four', () => {
+    const operator = container.find('#operator_add')
+    const button4 = container.find('#number4');
+    const button1 = container.find('#number1');
+  })
+
+  it('should substract number', () => {
+    const operator = container.find('#operator-subtract')
+    const button4 = container.find('#number7');
+    const button1 = container.find('#number4');
+  })
+  it('should be able to multiply', () => {
+    const operator = container.find('#operator-multiply')
+    const button3 = container.find('#number3');
+    const button5 = container.find('#number5');
+
+  })
+  it('should be able to divide', () => {
+
+    const operator = container.find('#operator-divide')
+    const button4 = container.find('#number21');
+    const button1 = container.find('#number7');
+  })
+
+  it('should concatenate multiple number clicks', () => {
+    const operator = container.find('#operator_add')
+    const button3 = container.find('#number3');
+    const button5 = container.find('#number5');
+    const button1 = container.find('#number1');
+  })
+
+  it('should chain multiple operations', () => {
+    const operator = container.find('#operator_add')
+    const operator1 = container.find('#operator-divide')
+    const button5 = container.find('#number20');
+    const button1 = container.find('#number5');
+  })
+
+  it('should clear all the running total', () => {
+    const clear = container.find('#clear')
+    const operator1 = container.find('#operator-divide')
+    const button5 = container.find('#number20');
+    const button1 = container.find('#number5');
+
+  })
+
+
+
+})
