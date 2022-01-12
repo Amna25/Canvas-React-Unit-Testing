b9aa8c030523475227d3087e0a08651465f0a474
Amna <amna.bashir2@gmail.com>
2022-01-12 22:27:28 +0000

    unit testing done

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -17,6 +17,7 @@ describe('Calculator', () => {
     button4.simulate('click');
     expect(runningTotal.text()).toEqual('4');
   })
+  // Add number 4 to 1
   it('should add one to four', () => {
     const button4 = container.find('#number4');
     const button1 = container.find('#number1');
@@ -29,47 +30,90 @@ describe('Calculator', () => {
     const runningTotal = container.find('#running-total');
     expect(runningTotal.text()).toEqual('5');
   })
+// Subtract 4 from 7
+  it('should substract number', () => {
+    const button7 = container.find('#number7');
+    const operator = container.find('#operator-subtract')
+    const button4 = container.find('#number4');
+    button7.simulate('click');
+    operator.simulate('click')
+    button4.simulate('click');
+    const equal = container.find('#operator-equals')
+    equal.simulate('click')
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('3');
+  })
+  // multiply 3 to 5
+  it('should be able to multiply', () => {
+    const operator = container.find('#operator-multiply')
+    const button3 = container.find('#number3');
+    const button5 = container.find('#number5');
+    button3.simulate('click');
+    operator.simulate('click')
+    button5.simulate('click');
+    const equal = container.find('#operator-equals')
+    equal.simulate('click')
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('15');
 
-  // it('should substract number', () => {
-  //   const operator = container.find('#operator-subtract')
-  //   const button4 = container.find('#number7');
-  //   const button1 = container.find('#number4');
-  // })
-  // it('should be able to multiply', () => {
-  //   const operator = container.find('#operator-multiply')
-  //   const button3 = container.find('#number3');
-  //   const button5 = container.find('#number5');
+  })
+  // Divide 21 to 7
+  it('should be able to divide', () => {
 
-  // })
-  // it('should be able to divide', () => {
+    const operator = container.find('#operator-divide')
+    const button8 = container.find('#number8');
+    const button4= container.find('#number4');
+    button8.simulate('click');
+    operator.simulate('click')
+    button4.simulate('click');
+    const equal = container.find('#operator-equals')
+    equal.simulate('click')
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('2');
+  })
+  // Concatination of multiple strings
+  it('should concatenate multiple number clicks', () => {
+    const button3 = container.find('#number3');
+    const button5 = container.find('#number5');
+    const button1 = container.find('#number1');
+    button3.simulate('click');
+    button5.simulate('click');
+    button1.simulate('click');
+    const equal = container.find('#operator-equals')
+    equal.simulate('click')
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('351');
+  })
+  // Multiple Operations togather
+  it('should chain multiple operations', () => {
+    const button6 = container.find('#number6');
+    const operator1 = container.find('#operator-divide')
+    const button3 = container.find('#number3');
+    const operator = container.find('#operator_add')
+    const button2 = container.find('#number2');
+    
+    button6.simulate('click');
+    operator1.simulate('click')
+    button3.simulate('click');
+    operator.simulate('click')
+    button2.simulate('click');
+ 
+    const equal = container.find('#operator-equals')
+    equal.simulate('click')
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('4');
+  })
 
-  //   const operator = container.find('#operator-divide')
-  //   const button4 = container.find('#number21');
-  //   const button1 = container.find('#number7');
-  // })
+  // Clear all running total
 
-  // it('should concatenate multiple number clicks', () => {
-  //   const operator = container.find('#operator_add')
-  //   const button3 = container.find('#number3');
-  //   const button5 = container.find('#number5');
-  //   const button1 = container.find('#number1');
-  // })
+  it('should clear all the running total', () => {
+    const button1 = container.find('#number5');
+    const clear = container.find('#clear')
 
-  // it('should chain multiple operations', () => {
-  //   const operator = container.find('#operator_add')
-  //   const operator1 = container.find('#operator-divide')
-  //   const button5 = container.find('#number20');
-  //   const button1 = container.find('#number5');
-  // })
+    button1.simulate('click')
+    clear.simulate('click')
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('0');
 
-  // it('should clear all the running total', () => {
-  //   const clear = container.find('#clear')
-  //   const operator1 = container.find('#operator-divide')
-  //   const button5 = container.find('#number20');
-  //   const button1 = container.find('#number5');
-
-  // })
-
-
-
+  })
 })
